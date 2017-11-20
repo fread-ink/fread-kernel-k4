8994b834755d57e51f05e230c8f687530545889d
Marc Juul <github@juul.io>
2017-11-20 03:05:08 -0800

    change output dir name

--- linux-2.6.31/build.sh
+++ linux-2.6.31/build.sh
@@ -12,12 +12,14 @@ depmod -ae -F System.map -b tar-install -r 2.6.31-rt11-lab126 -n > modules.dep
 
 sed -i 's/^kernel\//\/lib\/modules\/2.6.31-rt11-lab126\/kernel\//g' modules.dep
 
-mkdir -p COPY_TO_KINDLE
+mkdir -p OUTPUT
 
-cp arch/arm/boot/uImage COPY_TO_KINDLE/
-cp linux-2.6.31-rt11-lab126.tar.gz COPY_TO_KINDLE/
-cp modules.dep COPY_TO_KINDLE/
+cp arch/arm/boot/uImage OUTPUT/
+cp linux-2.6.31-rt11-lab126.tar.gz OUTPUT/
+cp modules.dep OUTPUT/
 
 echo " "
-echo "Build complete. Find the results in the COPY_TO_KINDLE dir."
-echo "It should contain one uImage, one tar.gz file and one modules.dep"
+echo "Build complete. Find the results in the OUTPUT dir."
+echo "It should contain one uImage (the kernel),"
+echo "one tar.gz containing the kernel modules"
+echo "and one modules.dep file."
